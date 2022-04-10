dbcb7b4d52a5cf8c44715ee4d4649fa5813d75c3
lae-laps <unenlagiaa@gmail.com>
2022-04-10 16:44:47 +0200

    added js

--- functions.js
+++ functions.js
@@ -1,3 +1,10 @@
 function text() {
     alert("hola");
 }
+
+function double() {
+    let x = document.getElementById("fname");
+    let d = x.value * 2;
+    document.getElementById("result").innerText=d;    
+}
+
